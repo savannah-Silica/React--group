8b7c79099f363e074a568c9b2646513274fa5da3
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-20 16:08:58 +0300

    contact page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import NavBar from './Components/Nav/NavBar';
 import Home from './Components/Pages/Home/Home';
+import Contact from './Components/Pages/Contact/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <div className='pages'>
-        <Home />
-      </div>
-    </div>
-  );
+  return (<>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </>)
+  // (
+  //   <div className="App">
+  //     <NavBar />
+  //     <div className="pages">
+  //       <Home />
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default App;
--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { contact } from "../../../data";
+import ContactForm from "./ContactForm";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <section className="section bg-primary h-auto ">
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:gap-x-8 lg:flex-row">
+            <div className="w-9/12">
+              <div className="flex flex-col ">
+                <h2 className="section-title ">
+                  Reach Out To Us
+                </h2>
+              </div>
+              <br />
+              {contact.map((item, index) => {
+                const { icon, title, description } = item;
+                return (
+                  <div
+                    className="flex flex-col lg:flex-row gap-x-4"
+                    key={index}
+                  >
+                    <div className=" rounded-sm w-14 h-14 flex items-start justify-center mt-2 mb-4 lg:mb-0 text-2xl">
+                      {icon}
+                    </div>
+                    <div>
+                      <h4 className="font-body text-xl mb-1">{title}</h4>
+                      <p className="text-accent font-normal ">{description}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <ContactForm />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
