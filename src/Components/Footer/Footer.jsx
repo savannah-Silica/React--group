8776b901b8291c8088e1514f4bce47ea0bd2fa80
amoskyalo <amoskyalo927@gmail.com>
2022-10-21 02:22:15 +0300

    worked on the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Footer.css'
 import logo from "../../Assets/logo.png";
 import { FaGithub, FaSlack, FaInstagram } from "react-icons/fa";
 import { CiLocationOn, CiTwitter } from "react-icons/ci";
@@ -7,106 +8,42 @@ import { SiGmail } from "react-icons/si";
 const Footer = () => {
   return (
     <footer>
-      <div className="con">
-        <div className="icons-con">
-          <div className="img-con">
-            <img src={logo} alt="" className="f1-logo" />
-            <h3>
-              SAVANNAH<br></br> SILICA
-            </h3>
-          </div>
-
-          <div className="i1">
-            <CiLocationOn />
-            <a href="#global" className="p">
-              <p>Global</p>
-            </a>
-          </div>
-
-          <div className="i1">
-            <FaGithub />
-            <a href="#savannahsilica" className="p">
-              <p>Savannah-Silica</p>
-            </a>
-          </div>
-
-          <div className="i1">
-            <FaSlack />
-            <a href="#SavannahSilica" className="p">
-              <p>Savannah-Silica</p>
-            </a>
-          </div>
-
-          <div className="i1">
-            <FaInstagram />
-            <a href="#savannatechies" className="p">
-              <p>@savannahtechies</p>
-            </a>
-          </div>
-
-          <div className="i1">
-            <SiGmail />
-            <a href="#savannaemail" className="p">
-              <p>savannahsilica@gmail.com</p>
-            </a>
-          </div>
-
-          <div className="i1">
-            <CiTwitter />
-            <a href="#savannatwitter" className="p">
-              <p>@savannahtechies</p>
-            </a>
-          </div>
+      <div className="divider"></div>
+      <div className="footer-logo">
+        <img src={logo} alt="" />
+      </div>
+      <div className="footer-links">
+        <div className="footer-link">
+          <p className="logo-link"><CiLocationOn /> Global</p>
+          <p className="logo-link"><FaGithub /> savannah-Silica</p>
+          <p className="logo-link"><FaSlack /> savannah-Silica</p>
+          <p className="logo-link"><FaInstagram /> @savannahtechies</p>
+          <p className="logo-link"><SiGmail /> savannah@gmail.com</p>
+          <p className="logo-link"><CiTwitter /> @savannahtechies</p>
         </div>
-
-        <div className="i2">
-          <h4>Commmunity</h4>
-          <a href="#blog">
-            <p>Blog</p>
-          </a>
-          <a href="#support">
-            <p>Support</p>
-          </a>
-          <a href="#privacypolicy">
-            <p>Privacy Policy</p>
-          </a>
-          <a href="#cookiepolicy">
-            <p>Cookie Policy</p>
-          </a>
-          <a href="#termsofservice">
-            <p>Terms of Service</p>
-          </a>
+        <div className="footer-link">
+          <h2 className="link-title">Community</h2>
+          <p>Blog</p>
+          <p>Support</p>
+          <p>Privacy policy</p>
+          <p>Cookie policy</p>
+          <p>Terms of service</p>
         </div>
-
-        <div className="i2">
-          <h4>About</h4>
-          <a href="#team">
-            <p>Team</p>
-          </a>
-          <a href="#projects">
-            <p>Projects</p>
-          </a>
-          <a href="#sponsors">
-            <p>Sponsors</p>
-          </a>
+        <div className="footer-link">
+          <h2 className="link-title">About</h2>
+          <p>Team</p>
+          <p>Projects</p>
+          <p>Sponsors</p>
         </div>
-
-        <div className="i2">
-          <h4>What we do</h4>
-          <a href="#uidesign">
-            <p>UI/Design</p>
-          </a>
-          <a href="#opensource">
-            <p>Open-Source</p>
-          </a>
-          <a href="#coding">
-            <p>Coding</p>
-          </a>
+        <div className="footer-link">
+          <h2 className="link-title">What we do</h2>
+          <p>UI/Design</p>
+          <p>Open source</p>
+          <p>Coding</p>
         </div>
       </div>
-
       <div className="copy">
-        <p>Copyright &copy; 2022 Savannah Silica. All rights reserved</p>
+        <p>Copyright @ 2022 Savannah Silica. All rights reserved</p>
       </div>
     </footer>
   );
