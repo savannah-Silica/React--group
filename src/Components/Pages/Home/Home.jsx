c77e9d67009eea80e7397b9a67df0fb2f3ba5ebc
salamah <salamahjimoh@gmail.com>
2022-10-21 20:41:15 -0700

    Add About-Intro Section

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../Header/Header'
 import Sponsors from '../../Sponsors/Sponsors'
 import Knowus from '../../KnowUS/Knowus'
+import About from '../About/About'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <Sponsors />
       <Knowus />
+      <About/>
     </div>
   )
 }
