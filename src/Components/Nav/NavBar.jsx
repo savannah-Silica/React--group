142f34c71ee45a00d935986f50127e5e4eeef9e7
tr1ms <gsonly.imo@gmail.com>
2022-10-24 19:22:11 +0300

    use NavLink instead for navigation links

--- src/Components/Nav/NavBar.jsx
+++ src/Components/Nav/NavBar.jsx
@@ -3,44 +3,39 @@ import logo from '../../Assets/logo.png'
 import './NavBar.css'
 import { BiMenu } from 'react-icons/bi'
 import { BsMoon } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const [active, setActive] = useState('#');
-  const [sticky, setSticky] = useState('');
+	const [isExpanded, setIsExpanded] = useState(false);
+	const [sticky, setSticky] = useState('');
 
-  //update sticky state
-  window.addEventListener('scroll', ()=>{
-    if(window.pageYOffset > 80){
-         setSticky("sticky");
-    }else setSticky("");
-  })
+	//update sticky state
+	window.addEventListener('scroll', () => {
+		if (window.pageYOffset > 80) {
+			setSticky("sticky");
+		} else setSticky("");
+	})
 
-  return (
-     <header className={`header ${sticky === "sticky" ? `sticky` : ""}`}>
-        <div className="logo">
-            <img src={logo} alt="logo" />   
-        </div>
+	return (
+		<header className={`header ${sticky === "sticky" ? `sticky` : ""}`}>
+			<div className="logo">
+				<img src={logo} alt="logo" />
+			</div>
 
-        <nav className={`nav-links ${isExpanded ? `nav-toggled` : ""}`}>
-            <a className={`nav-link ${active === '#' ? `active` : ''}`} href="/"
-            onClick={()=>setActive("#")}>Home</a>
-            <a className={`nav-link ${active === '1' ? `active` : ''}`} href="/"
-            onClick={()=>setActive("1")}>About Us</a>
-            <a className={`nav-link ${active === '2' ? `active` : ""}`} href="/"
-            onClick={()=>setActive('2')}>Services</a>
-            <a className={`nav-link ${active === '3' ? `active` : ""}`} href="/"
-            onClick={()=>setActive('3')}><Link to="/ContactUs">Contact Us</Link></a>
-        </nav>
+			<nav className={`nav-links ${isExpanded ? `nav-toggled` : ""}`}>
+				<NavLink className="nav-link" to="/home">Home</NavLink>
+				<NavLink className="nav-link" to="/about">About Us</NavLink>
+				<NavLink className="nav-link" to="/services">Services</NavLink>
+				<NavLink className="nav-link" to="/contacts">Contact Us</NavLink>
+			</nav>
 
-        <div className="nav-buttons">
-            <BsMoon className='nav-icons theme-toggle'/>
-            <button className='btn'>Sign In</button>
-            <BiMenu className='nav-icons nav-toggle' onClick={ () => setIsExpanded(!isExpanded)}/>
-        </div>
-    </header>
-  )
+			<div className="nav-buttons">
+				<BsMoon className='nav-icons theme-toggle' />
+				<button className='btn'>Sign In</button>
+				<BiMenu className='nav-icons nav-toggle' onClick={() => setIsExpanded(!isExpanded)} />
+			</div>
+		</header>
+	)
 }
 
 export default NavBar
